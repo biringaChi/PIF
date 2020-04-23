07e44ca7bedb5f8caaf3a62f23ae0bc48d4671d4
Biringa Chidera <biringachidera@gmail.com>
2020-04-23 16:00:04 -0400

    Modifications

--- performance_measure.py
+++ performance_measure.py
@@ -3,18 +3,8 @@ __author__ = "biringaChi"
 import numpy as np
 
 class PerformanceMeasures():
-    def __init__(self, img1, img2):
-        self.img1 = img1
-        self.img2 = img2
-
-    def pixel_distance(self):
-        if len(self.img1) != len(self.img2) and len(self.img1[0]) != len(self.img2[0]):
-            raise Exception("Image dimensions are different")
-        else:
-            dist_measure = np.sum((self.img1 - self.img2) ** 2)
-            dist_measure /= float(self.img1.shape[0] * self.img1.shape[1])
-            return dist_measure
-
+    pass
 
 if __name__ == '__main__':
     pd = PerformanceMeasures()
+
