02464e6858af05e29389a60b6300e345d5776d3e
Biringa Chidera <biringachidera@gmail.com>
2020-04-25 13:57:43 -0400

    Edits

--- data_prep.py
+++ data_prep.py
@@ -1,9 +1,9 @@
 __author__ = 'biringaChidera'
 
 import torch
-import torch.nn as nn
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+
 
 class DataPrep():
     def __init__(self, directory, batch_size):
@@ -12,14 +12,14 @@ class DataPrep():
 
     def training_data_prep(self):
         image_size = 50
-        dataset = dset.ImageFolder(root = self.directory,
-            transform = transforms.Compose([
+        dataset = dset.ImageFolder(root=self.directory,
+            transform=transforms.Compose([
                 transforms.Resize(image_size),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                 ]))
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size = self.batch_size, shuffle=True)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
         return dataloader
 
 
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     directory, batch_size = "data", 128
     training_data = DataPrep(directory, 128)
     training_batch = next(iter(training_data.training_data_prep()))
-    #print(training_batch)
+#    print(training_batch)
--- generator.py
+++ generator.py
@@ -1,31 +1,31 @@
 __author__ = 'biringaChidera'
 
-import torch
 import torch.nn as nn
 
+
 class Generator(nn.Module):
-    def __init__(self, nc, ndf):
+    def __init__(self, nc, ngf):
         super(Generator, self).__init__()
         self.nc = nc
-        self.ndf = ndf
+        self.ngf = ngf
 
         self.preprocess = nn.Sequential(
-            nn.Conv2d(nc, ndf, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
-            nn.Conv2d(ndf, ndf * 2, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True))
+            nn.Conv2d(nc, ngf, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
+            nn.Conv2d(ngf, ngf * 2, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True))
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(ndf * 2, ndf * 4, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
-            nn.Conv2d(ndf * 4, ndf * 8, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
-            nn.Conv2d(ndf * 8, ndf * 16, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
-            nn.Conv2d(ndf * 16, ndf * ndf, kernel_size=4, stride=1, bias=False), nn.ReLU(True))
+            nn.Conv2d(ngf * 2, ngf * 4, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
+            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
+            nn.Conv2d(ngf * 8, ngf * 16, kernel_size=4, stride=2, padding=1, bias=False), nn.ReLU(True),
+            nn.Conv2d(ngf * 16, ngf * ngf, kernel_size=4, stride=1, bias=False), nn.ReLU(True))
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(ndf * ndf, ndf * 16, kernel_size=4, stride=1, bias=False), nn.BatchNorm2d(ndf * ndf), nn.ReLU(True),
-            nn.ConvTranspose2d(ndf * 16, ndf * 8, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ndf * 16), nn.ReLU(True),
-            nn.ConvTranspose2d(ndf * 8, ndf * 4, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ndf * 8), nn.ReLU(True),
-            nn.ConvTranspose2d(ndf * 4, ndf * 2, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ndf * 4), nn.ReLU(True),
-            nn.ConvTranspose2d(ndf * 2, ndf, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ndf * 2), nn.ReLU(True),
-            nn.ConvTranspose2d(ndf, nc, kernel_size=4, stride=2, padding=1, bias=False), nn.Tanh())
+            nn.ConvTranspose2d(ngf * ngf, ngf * 16, kernel_size=4, stride=1, bias=False), nn.BatchNorm2d(ngf * ngf), nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ngf * 16), nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ngf * 8), nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, padding=1, bias=False), nn.BatchNorm2d(ngf * 2), nn.ReLU(True),
+            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False), nn.Tanh())
 
     def forward(self, img1, img2):
         img = self.preprocess(img1) + self.preprocess(img2)
@@ -33,14 +33,7 @@ class Generator(nn.Module):
         img = self.decoder(img)
         return img
 
+
 if __name__ == '__main__':
-    gen = Generator()
-    print(gen)
-
-
-
-
-
-
-
-
+    gen = Generator(3, 32)
+#   print(gen)
