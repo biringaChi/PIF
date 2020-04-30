3621345ab7c6b9a2125c4f3621097a51ec6987c0
rjthompson22 <9561786+rjthompson22@users.noreply.github.com>
2020-04-30 19:45:45 -0400

    Add files via upload

---
+++ ImagesToVideo.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import os
+from os.path import isfile, join
+
+pathIn= './images/testing/'
+pathOut = 'videoRsult.mp4'
+
+fps = 0.5
+frame_array = []
+files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+
+#for sorting the file names properly
+files.sort(key = lambda x: x[5:-4])
+files.sort()
+frame_array = []
+files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+
+#for sorting the file names properly
+files.sort(key = lambda x: x[5:-4])
+
+for i in range(len(files)):
+    filename=pathIn + files[i]
+    #reading each files
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    
+    #inserting the frames into an image array
+    frame_array.append(img)
+out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+for i in range(len(frame_array)):
+    # writing to a image array
+    out.write(frame_array[i])
+
+out.release()
---
+++ YoutubeDownloader.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 19:39:49 2020
+
+@author: rjsem
+"""
+
+#Need to pip install pytube
+
+from pytube import YouTube
+
+URL = "https://www.youtube.com/watch?v=nSdz5ln2rME"
+
+yt = YouTube(URL)
+yt = yt.get('mp4', '720p')
+
+yt.download('/path/to/download/directory')
