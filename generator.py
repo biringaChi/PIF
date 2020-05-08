513d79f39dce28c4a747019cf8c4264a3210e3af
Josue N Rivera <josue.n.rivera@outlook.com>
2020-05-08 14:39:56 -0400

    Padding issue with image size

--- generator.py
+++ generator.py
@@ -12,38 +12,38 @@ class Generator(nn.Module):
         self.ngf = ngf
 
         self.preprocess = nn.Sequential(
-            nn.Conv2d(nc, ngf, kernel_size=4, stride=2, bias=False),
+            nn.Conv2d(nc, ngf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.ReLU(True),
-            nn.Conv2d(ngf, ngf * 2, kernel_size=4, stride=2, bias=False),
+            nn.Conv2d(ngf, ngf * 2, kernel_size=4, stride=2, padding=1, bias=False),
             nn.ReLU(True))
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(ngf * 2, ngf * 4, kernel_size=4, stride=2, bias=False),
+            nn.Conv2d(ngf * 2, ngf * 4, kernel_size=4, stride=2, padding=1, bias=False),
             nn.ReLU(True),
-            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=4, stride=2, bias=False),
+            nn.Conv2d(ngf * 4, ngf * 8, kernel_size=4, stride=2, padding=1, bias=False),
             nn.ReLU(True),
-            nn.Conv2d(ngf * 8, ngf * 16, kernel_size=4, stride=2, bias=False),
+            nn.Conv2d(ngf * 8, ngf * 16, kernel_size=4, stride=2, padding=1, bias=False),
             nn.ReLU(True),
-            nn.Conv2d(ngf * 16, ngf * ngf, kernel_size=4, stride=1, bias=False),
+            nn.Conv2d(ngf * 16, ngf * ngf, kernel_size=4, stride=1, padding=1, bias=False),
             nn.ReLU(True))
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(ngf * ngf, ngf * 16, kernel_size=4, stride=1, bias=False),
+            nn.ConvTranspose2d(ngf * ngf, ngf * 16, kernel_size=4, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, kernel_size=4, stride=2, bias=False),
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, bias=False),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, bias=False),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, bias=False),
+            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False),
             nn.Tanh())
 
     def forward(self, img1, img2):
